9b94bbbd00e5d704c4c3ac723209d71094b9addb
spiner721 <spinerspinerreal@gmail.com>
2026-03-05 17:36:28 +0200

    oauth2 fix

--- simple_test.py
+++ simple_test.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test')
-def test():
-    return "Test route working!"
-
-@app.route('/recent_tab_roulette')
-def recent_tab_roulette():
-    return "Roulette route working!"
+@app.route('/')
+def index():
+    return "<h1>Test Server Working!</h1><p>If you see this, Flask is working correctly.</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10001)
+    app.run(debug=True, host='0.0.0.0', port=10001)
